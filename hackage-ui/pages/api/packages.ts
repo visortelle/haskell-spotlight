c1c76d2237ccd676f7d3d4adf2737e4022e11260
visortelle <visortelle@gmail.com>
2022-01-01 10:37:40 +0100

    Continue working on Home page

--- hackage-ui/pages/api/packages.ts
+++ hackage-ui/pages/api/packages.ts
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+// type State = {
+//   packages:
+// }
+
 type Data = {
   name: string
 }
