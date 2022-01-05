417fd0fb3681143c55829bdbc88769cdb5ffda8c
visortelle <visortelle@gmail.com>
2022-01-05 19:02:16 +0100

    Fix page scaling on mobile

--- hackage-ui/pages/_app.tsx
+++ hackage-ui/pages/_app.tsx
@@ -5,8 +5,7 @@ import { DefaultAppContextProvider } from '../components/AppContext';
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
     <Head>
-      {/* Show desktop version on mobile devices until we support it. */}
-      {/* <meta name="viewport" content="width=1100, initial-scale=1, maximum-scale=1 shrink-to-fit=no"></meta> */}
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
     </Head>
 
     <DefaultAppContextProvider>
