6057e071a7cd5ed953589a486210b0c11c85db7c
visortelle <visortelle@gmail.com>
2022-01-05 10:16:20 +0100

    Cleanup page footer

--- hackage-ui/components/layout/Footer.tsx
+++ hackage-ui/components/layout/Footer.tsx
@@ -6,34 +6,22 @@ const Footer = () => {
       <div className={s.footerContent}>
         <div className={s.footerColumn}>
           <h3 className={s.footerColumnHeader}>Haskell</h3>
-          <a href="#">hackage.haskell.org</a>
-          <a href="#">Privacy Policy</a>
-          <a href="#">Terms & Conditions</a>
-          <a href="mailto:contact@haskell.org">contact@haskell.org</a>
+          -
         </div>
 
         <div className={s.footerColumn}>
           <h3 className={s.footerColumnHeader}>Get Help</h3>
-          <a href="#">hackage.haskell.org</a>
-          <a href="#">Privacy Policy</a>
-          <a href="#">Terms & Conditions</a>
-          <a href="mailto:contact@haskell.org">contact@haskell.org</a>
+          -
         </div>
 
         <div className={s.footerColumn}>
           <h3 className={s.footerColumnHeader}>Policies</h3>
-          <a href="#">hackage.haskell.org</a>
-          <a href="#">Privacy Policy</a>
-          <a href="#">Terms & Conditions</a>
-          <a href="mailto:contact@haskell.org">contact@haskell.org</a>
+          -
         </div>
 
         <div className={s.footerColumn}>
           <h3 className={s.footerColumnHeader}>Social</h3>
-          <a href="#">haskell/hackage</a>
-          <a href="#">Privacy Policy</a>
-          <a href="#">Terms & Conditions</a>
-          <a href="mailto:contact@haskell.org">contact@haskell.org</a>
+          -
         </div>
       </div>
     </footer>
