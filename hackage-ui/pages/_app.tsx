262ee32fa3fa77365148261062d0b3ef4c9cbc29
visortelle <visortelle@gmail.com>
2022-01-05 10:47:20 +0100

    Show desktop vesion on mobile devices wihle we don't support it

--- hackage-ui/pages/_app.tsx
+++ hackage-ui/pages/_app.tsx
@@ -4,6 +4,8 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
     <Head>
+      {/* Show desktop version on mobile devices until we support it. */}
+      <meta name="viewport" content="width=1100, initial-scale=1, maximum-scale=1 shrink-to-fit=no"></meta>
     </Head>
     <Component {...pageProps} />
   </>
